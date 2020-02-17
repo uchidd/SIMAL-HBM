e8f327a84ecab32d3a8cd59e020b0b8ac41ee575
uchidd <mmi090903@gmail.com>
2020-02-17 17:07:09 +0700

    Fourth Commit

---
+++ pages/DashboardPage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+export default class DashboardPage extends Component{
+
+  static navigationOptions = {
+    drawerLabel: "Dashboard",
+  };
+
+
+  render(){
+    return(
+      <View style={styles.container}>
+
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
