916fb29e805092d72045138fc8d05579b5e9872b
uchidd <mmi090903@gmail.com>
2020-02-17 16:42:48 +0700

    Third Commit

--- App.js
+++ App.js
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import DrawerNavigator from 'react-navigation-drawer/lib/typescript/src/navigators/createDrawerNavigator';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component{
+  render(){
+    return(
+      <View style={styles.container}>
+        <DrawerNavigator/>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
