6f943836525f29925760968f319865cccc25fa4c
rkkhaerul <rkkhaerul@gmail.com>
2020-02-17 17:07:04 +0700

    17:06, 17/02/2020

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import DrawerNavigator from 'react-navigation-drawer';
+import DrawerNavigator from './Navigator/DrawerNavigator'
 
 export default class App extends Component{
   render(){
